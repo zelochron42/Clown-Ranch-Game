c29f599450196ec2652ce0576f63f8a78b2bb5e4
OreoEP98 <55422167+OreoEP98@users.noreply.github.com>
2024-01-27 01:29:22 -0500

    Eric update
    Timer added in separate eric scene. Carrt sprite and ground tilemap added to sprite project folder

---
+++ UITimer.gd
@@ -0,0 +1,31 @@
+extends Node
+
+@onready var label = $Label #Reference to the lable node
+@onready var timer = $Timer #Reference to timer node
+
+
+var time_left
+var minute 
+var second
+
+func _ready():
+	timer.start()
+	
+func time_left_counter():
+	time_left = timer.time_left
+	minute = floor(time_left / 60)
+	second = int(time_left) % 60
+	return[minute,second]
+	
+
+func _process(delta):
+	label.text="%02d:%02d" % time_left_counter()
+	#Anycode below this is what triggers when time hits 0:00
+	#Id imagine a switch statement or even a random number gen for spawning enamies during the wave
+	if second==0 and minute ==0:
+		print("This is when an event must happen")
+	
+#Only thing Now is resetting the timer when loading a new scene
+
+func _on_timer_timeout():
+	pass # Replace with function body.
